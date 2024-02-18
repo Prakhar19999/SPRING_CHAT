02a96de97a3d749bfc2fc8469135c1b27f0c0144
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2024-02-18 09:57:49 +0530

    WebSocket configuration

--- src/main/java/com/spring/chat/config/WebSocketConfig.java
+++ src/main/java/com/spring/chat/config/WebSocketConfig.java
@@ -1,5 +1,23 @@
 package com.spring.chat.config;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
-public class WebSocketConfig {
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
+{
+    //Register STOMP endpoints mapping each to a specific URL and (optionally) enabling and configuring SockJS fallback options.
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry)
+    {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+    //Configure message broker options.
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry)
+    {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
 }
